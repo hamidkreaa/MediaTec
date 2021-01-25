2b5a751f0ca8def97a5dbb71c4d6c12c8bfa066b
Abdel Hamid Kreaa <a.kreaa@pkn.de>
2021-01-25 11:11:28 +0100

    Apply AnnotationsToCustomerName -Add migration/ Update DB

--- MediaTec/Models/Customer.cs
+++ MediaTec/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MediaTec.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         public bool IsSubscriebedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
