ba9c7367739d627350080f0522f6f27b9e39150a
Abdel Hamid Kreaa <a.kreaa@pkn.de>
2021-01-25 09:26:54 +0100

    Move NavBar to Partial View

--- MediaTec/App_Start/RouteConfig.cs
+++ MediaTec/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace MediaTec
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               "MoviesByReleaseDate",
+               "movies/released/{year}/{month}",
+               new { Controller = "Movies", action = "ByReleaseDate" },
+               new { year = @"\d{4}", month = @"\d{2}" }
+          // new { year = @"2015|2016", month = @"\d{2}" }
+          );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
