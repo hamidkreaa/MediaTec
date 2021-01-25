1e46931c464a50a7461d933819dc71ee38028d9a
Abdel Hamid Kreaa <a.kreaa@pkn.de>
2021-01-25 13:54:28 +0100

    Add Genre Model - Add Attributes to Movie Model / add-migration , update DB

---
+++ MediaTec/Migrations/202101251235037_AddAttributesToMovie.cs
@@ -0,0 +1,38 @@
+namespace MediaTec.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttributesToMovie : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "AddDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Short(nullable: false));
+            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropColumn("dbo.Movies", "GenreId");
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropColumn("dbo.Movies", "AddDate");
+            DropColumn("dbo.Movies", "ReleaseDate");
+            DropTable("dbo.Genres");
+        }
+    }
+}
