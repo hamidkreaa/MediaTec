2a94d6fc990c5967ac99c1df62b3ba91731c4715
Abdel Hamid Kreaa <a.kreaa@pkn.de>
2021-01-25 08:49:14 +0100

    Add Customers and Movies Models and Controllers

---
+++ MediaTec/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using MediaTec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaTec.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //public List<Customer> Customers;
+
+        //public CustomersController()
+        //{
+
+        //     Customers = new List<Customer>();
+        //    //{
+        //    //    new Customer{Id =1, Name = "Hamid Kreaa"},
+        //    //    new Customer{Id =2, Name = "Farah Kreaa"}
+        //    //};
+        //}
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var Customers = GetCustomers();
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Hamid Kreaa" },
+                new Customer { Id = 2, Name = "Farah Kreaa" }
+            };
+        }
+    }
+}
---
+++ MediaTec/Controllers/MoviesController.cs
@@ -0,0 +1,86 @@
+﻿using MediaTec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaTec.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> Movies;
+
+
+        public MoviesController()
+        {
+            Movies = new List<Movie> {
+                new Movie{Id = 1, Name = "Shrek!"},
+                new Movie{Id = 2, Name = "Wall-e"},
+            };
+
+        }
+
+        // GET: Movies
+       
+
+        // return Content("Hello Hamid");
+        // return HttpNotFound();
+        // return new EmptyResult();
+        // return RedirectToAction("Index", "Home" , new {page = 1, sortBy = "name"});
+
+        //ViewResult -> View()
+        //PatialViewResult -> PartialView()
+        //ContentResult -> Content()
+        //RedirectResult -> Redirect()
+        //RedirecToRoutetResult -> RedirectToAction()
+        //JsonResult -> Json()
+        //FileResult -> File()
+        //HttpNotFoundResult -> HttpNotFound()
+        //EmptyResult -> EmptyResult()
+
+
+        // http://localhost:52273/movies/edit/5
+        //http://localhost:52273/movies/edit?id=7
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}): range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //http://localhost:52273/movies?Pageindex=2&sortby=Hamid
+        public ActionResult index(int? pageIndex, string sortBy)
+
+        {
+            //var movies = new List<Movie> {
+            //    new Movie{Id = 1, Name = "Shrek!"},
+            //    new Movie{Id = 2, Name = "Wall-e"},
+            //};
+
+            return View(Movies);
+
+            //if (!pageIndex.HasValue)
+            //    pageIndex = 1;
+            //if (string.IsNullOrWhiteSpace(sortBy))
+            //    sortBy = "Name";
+            //return Content(string.Format("PageIndex={0}&sortBy{1}", pageIndex,sortBy));
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = Movies.Find(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+    }
+}
